a1611a1b849f642d6186824115c353e36ae6ac5c
berru-g <g.leberruyer@gmail.com>
2025-10-01 13:09:19 +0200

    majdeco3D

--- 3D/script3.js
+++ 3D/script3.js
@@ -551,7 +551,7 @@ function loadEnvironmentGLB() {
         //'https://raw.githubusercontent.com/berru-g/berru-g/refs/heads/main/img/space.glb', //"Map_tkgcz" (https://skfb.ly/pyOyZ) by amogusstrikesback2 is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
         //'https://raw.githubusercontent.com/berru-g/crypto-tool/main/heatmap-forest/assets/iss.glb',
         //'https://raw.githubusercontent.com/berru-g/3d-scroll-animate/main/assets/....glb',
-        'https://raw.githubusercontent.com/berru-g/berru-g/refs/heads/main/img/flying_island.glb', //"Flying Island - Low Poly" (https://skfb.ly/6v6y8) by ChojoThePony is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+        'https://raw.githubusercontent.com/berru-g/berru-g/refs/heads/main/img/drone.glb', //"Flying Island - Low Poly" (https://skfb.ly/6v6y8) by ChojoThePony is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
         
 
     ];
