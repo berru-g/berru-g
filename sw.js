964bcac8de2406d75703dc7dab72b27fcc338e11
berru-g <g.leberruyer@gmail.com>
2026-03-14 19:53:14 +0100

    pwa on

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
 const CACHE_NAME = 'SP_LibreAnalytics';
 const urlsToCache = [
-  '/smart_pixel_v2/public/login.php',
-  '/smart_pixel_v2/public/dashboard.php',
-  '/smart_pixel_v2/assets/dashboard.css',
-  '/smart_pixel_v2/assets/icons/icon-192x192.png',
-  '/smart_pixel_v2/assets/icons/icon-512x512.png',
+  '/LibreAnalytics/smart_pixel_v2/public/login.php',
+  '/LibreAnalytics/smart_pixel_v2/public/logout.php',
+  '/LibreAnalytics/smart_pixel_v2/public/dashboard.php',
+  '/LibreAnalytics/smart_pixel_v2/public/account.php',
+  '/LibreAnalytics/smart_pixel_v2/assets/dashboard.css',
+  '/LibreAnalytics/smart_pixel_v2/assets/login.css',
+  '/LibreAnalytics/smart_pixel_v2/assets/icons/icon-192x192.png',
+  '/LibreAnalytics/smart_pixel_v2/assets/icons/icon-512x512.png',
   'https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js',
   'https://cdn.jsdelivr.net/npm/sweetalert2@11'
 ];
